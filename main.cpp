5b75edb44555a965a0630dff42d3de3452e75e1a
ejimm2015 <moh.lam9596@gmail.com>
2020-05-27 00:37:41 +0200

    testing 3DES within main method

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@
 #include "DES.h"
 #include "Crypt.h"
 #include "Decrypt.h"
-#include "DESinv.h"
 
 int main() {
     SequenceD<64> key1 = SequenceD<64>();
     operator>>(std::cin, key1);
-    std::cout << "Key : ";
+    SequenceD<64> key2 = SequenceD<64>();
+    operator>>(std::cin, key2);
+    std::cout << "Key1 : ";
     affichage(key1);
+    std::cout << std::endl << "=======" << std::endl;
+    std::cout << "Key2 : ";
+    affichage(key2);
     std::cout << std::endl << "=======" << std::endl;
     SequenceD<64> x = SequenceD<64>();
     operator>>(std::cin, x);
@@ -36,5 +40,19 @@ int main() {
     affichage(decrypt);
     std::cout << std::endl;
     operator<<(std::cout, decrypt);
+
+    Crypt crypt1 = Crypt(key1, key2);
+    SequenceD<64> cyphered = crypt1(x);
+    std::cout << std::endl << "Cyphered=======" << std::endl;
+    operator<<(std::cout, cyphered);
+    std::cout << std::endl;
+    Decrypt decrypt1 = Decrypt(key1, key2);
+    SequenceD<64> decyphered = decrypt1(cyphered);
+    std::cout << std::endl << "Decyphered=======" << std::endl;
+    operator<<(std::cout, decyphered);
+    std::cout << std::endl;
+
+
+
     return 0;
 }
